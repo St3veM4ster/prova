fa8cbd7519b14c2c7f2c5bafd97028ff5aea26cc
St3veM4ster <teven.1492@hotmail.com>
2018-08-09 16:37:28 +0200

    primo commento

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int num = 34;
+int num2 = 5;
+int sum = num * num2;
+using namespace std;
+
+string letra = "Dios Gracias";
+bool variable;
+
+int main()
+{
+    float nu = 65.5678975682;
+    float xd = (float) nu + num2;
+    variable = (num > num2 || sum > num2);
+
+    cout << variable << "\n" << xd << "\n" << nu << "\n"  << num << "\n" <<  num2 << "\n" << sum << "\n" << endl ;
+
+
+    return 0;
+}
